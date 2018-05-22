2b8167286ef838eccd84daa1ec9abc76c48016cf
Lay <tonglay1219@gmail.com>
2018-05-21 19:07:45 -0600

    Added dependency for okhttp and worked on the login implementation

--- FrontEnd/app/src/main/java/pro200/neumont/edu/externaljournal/Activities/LoginActivity.java
+++ FrontEnd/app/src/main/java/pro200/neumont/edu/externaljournal/Activities/LoginActivity.java
@@ -5,14 +5,19 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import pro200.neumont.edu.externaljournal.Helper.DataGetter;
+import pro200.neumont.edu.externaljournal.Helper.HttpRequestHelper;
 import pro200.neumont.edu.externaljournal.Helper.LoginValidationObj;
 import pro200.neumont.edu.externaljournal.Helper.RequestHelper;
 import pro200.neumont.edu.externaljournal.R;
+
+import java.io.IOException;
 
 public class LoginActivity extends AppCompatActivity {
     private Button mLoginButton;
     private EditText mUsernameEditText;
     private EditText mPasswordEditText;
+    private LoginValidationObj validationObj;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,14 +32,36 @@ public class LoginActivity extends AppCompatActivity {
             System.out.println("mUsernameEditText - " + mUsernameEditText.getText().toString());
             System.out.println("mPasswordEditText - " + mPasswordEditText.getText().toString());
 
-            LoginValidationObj loginObj =
-                    RequestHelper.doLogin(mUsernameEditText.getText().toString().trim(), mPasswordEditText.getText().toString().trim());
+//            LoginValidationObj loginObj = null;
+//            try {
+//                loginObj = DataGetter.LoginValidation(
+//                        mUsernameEditText.getText().toString().trim(),
+//                        mPasswordEditText.getText().toString().trim());
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
 
-            String message = "";
-            if(!loginObj.isSuccess())
-                message = loginObj.getMessage();
+            doLogin(mUsernameEditText.getText().toString().trim(), mPasswordEditText.getText().toString().trim());
+//            String message = validationObj.isSuccess() ? validationObj.getMessage() : "Failed";
 
-            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+//            Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
         });
+    }
+
+    private void doLogin(String username, String password) {
+        Thread thread = new Thread(() -> runOnUiThread(() -> {
+            try {
+                LoginValidationObj loginObj = DataGetter.LoginValidation(
+                        mUsernameEditText.getText().toString().trim(),
+                        mPasswordEditText.getText().toString().trim());
+                System.out.println(loginObj.getMessage() + " - " + loginObj.isSuccess());
+
+                Toast.makeText(this, loginObj.getMessage(), Toast.LENGTH_SHORT).show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }));
+
+        thread.start();
     }
 }
