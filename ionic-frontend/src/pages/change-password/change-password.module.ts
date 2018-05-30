2b4ad018757c16b7acffacd28b1e6578e31ccd5f
Unknown <isaiahcjc5@gmail.com>
2018-05-29 18:40:59 -0600

    Auto stash before revert of "Setting page"

---
+++ ionic-frontend/src/pages/change-password/change-password.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core'; 
+import { IonicPageModule } from 'ionic-angular'; 
+ 
+import { ChangePassword } from './change-password'; 
+ 
+@NgModule({ 
+    declarations: [ 
+      ChangePassword, 
+    ], 
+    imports: [ 
+      IonicPageModule.forChild(ChangePassword) 
+    ], 
+    exports: [ 
+      ChangePassword 
+    ] 
+  }) 
+  export class ChangePasswordPageModule { }
