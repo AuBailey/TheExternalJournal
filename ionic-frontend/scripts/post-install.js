28402fb3dc1dc741dbb227ba481e0c5b64c05c11
Brian Weasner <weasnerb@gmail.com>
2018-06-06 17:31:31 -0600

    Post npm install script fixed.

--- ionic-frontend/scripts/post-install.js
+++ ionic-frontend/scripts/post-install.js
@@ -6,15 +6,19 @@ function postInstall()  {
 
 function deleteNgxBootstrapDatepickerCss() {
   filepaths = [
-    '../node_modules/ngx-bootstrap/datepicker/utils/scss/mixins.css',
-    '../node_modules/ngx-bootstrap/datepicker/utils/scss/variables.css'
+    './node_modules/ngx-bootstrap/datepicker/utils/scss/mixins.css',
+    './node_modules/ngx-bootstrap/datepicker/utils/scss/variables.css'
   ]
   deleteFiles(filepaths);
 };
 
 function deleteFiles(filepaths) {
-  for (let filepath in filepaths) {
-    fs.unlinkSync(filepath)
+  for (let filepath of filepaths) {
+    try {
+      fs.unlinkSync(filepath);
+    } catch (error) {
+      console.error(error);
+    }
   }
 }
 
